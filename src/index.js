073da9b3cf093211c54bc278a020fdff8ff9a176
Víctor Auñón <hola@victoraunon.com>
2022-07-19 14:10:46 +0200

    Switching ReactDOM.render to createRoot

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 
 import { BrowserRouter as Router } from "react-router-dom";
 
-
 import App from "./App";
 
-ReactDOM.render(
-    <Router>
-        <App />
-    </Router>,
-    document.getElementById("root")
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <Router>
+    <App />
+  </Router>,
 );
