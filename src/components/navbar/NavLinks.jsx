73d9b65b9a088a989b69b27b330a10cb7501ca66
Víctor Auñón <hola@victoraunon.com>
2022-08-01 02:40:33 +0200

    Home link in navbar links

--- src/components/navbar/NavLinks.jsx
+++ src/components/navbar/NavLinks.jsx
@@ -1,15 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const NavLinks = props => {
-
   const justify = props.justify ? props.justify : undefined;
 
   return (
     <nav>
       <ul className={`flex items-center gap-8 ${justify}`}>
-        <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
-          Home
-        </li>
+        <Link to={"/"}>
+          <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+            Home
+          </li>
+        </Link>
         <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
           Menu
         </li>
